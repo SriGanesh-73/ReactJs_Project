f21f9d891a3176c3846b312c8d802a17d97e2735
SriGanesh-73 <sriganeshthiyagarajan@gmail.com>
2025-03-30 19:55:55 +0530

    Update AllRouters.jsx

--- dental_hospital/src/AllRouters.jsx
+++ dental_hospital/src/AllRouters.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './index.jsx';
 import AboutUs from './About_Us';
 import ContactUs from './contact.jsx';
